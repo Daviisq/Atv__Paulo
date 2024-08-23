765a402e138b41178509a749607297f77a48d875
Daviisq <davi.domingos@etec.sp.gov.br>
2024-08-23 15:34:50 -0300

    first commit

---
+++ Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_16_08
+{
+    internal class Pessoa
+    {
+        public string nome;
+
+        public string perguntarnome() 
+        {
+            string nome;
+            Console.WriteLine("Qual seu nome?");
+            nome=Console.ReadLine();
+            return nome;
+        }
+    }
+}
