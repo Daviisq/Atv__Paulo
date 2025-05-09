41bb6a5b7e4f87ebef710f2132baa03e41dfe4f7
Daviisq <davi.domingos@etec.sp.gov.br>
2025-05-09 15:34:34 -0300

    Finalizado

---
+++ pessoa.php
@@ -0,0 +1,26 @@
+<?php
+    class Pessoa{   
+        private $cpf, $nome, $contato;
+
+        public function getCpf(){
+            return $this->cpf;
+        }
+        public function setCpf($cpf){
+            $this->cpf = $cpf;
+        }
+
+        public function getNome(){
+            return $this->nome;
+        }
+        public function setNome($nome){
+            $this->nome = $nome;
+        }
+
+        public function getContato(){
+            return $this->contato;
+        }
+        public function setContato($contato){
+            $this->contato = $contato;
+        }
+    }
+?>  
